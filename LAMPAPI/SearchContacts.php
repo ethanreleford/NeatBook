9fc90963880a2b70cfe95fb5162ec3430d5e16da
Ryan Weisman <ryan.weisman.512@gmail.com>
2023-09-01 12:41:36 -0400

    Update SearchContacts.php
    Updated bind parameters

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -14,7 +14,7 @@
 	{
 		$stmt = $conn->prepare("select * from Contacts where (FirstName like ? or LastName like ?) and UserID=?");
 		$contactName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $contactName, $contactName, $inData["userId"]);
+		$stmt->bind_param("sss", $contactName, $contactName, $inData["userId"]);
 		$stmt->execute();
 		
 		$result = $stmt->get_result();
