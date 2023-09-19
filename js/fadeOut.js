22972c8a216e4170b2b2c20dbbf1e9f2d6ca5e9f
Declan Carter <declanpcarter@gmail.com>
2023-09-19 17:43:09 -0400

    Resolve Issues from fadeOut.js Changes

--- js/fadeOut.js
+++ js/fadeOut.js
@@ -1,6 +1,6 @@
 
 function fadeOut(link){
-    document.getElementById("fadeinWrapper").style.animation = "fadeOut 1s";
+    document.getElementById("fadeinWrapper").style.animation = "fadeOut 1s forwards";
     setTimeout(function()
     {
         document.getElementById("myForm").style.animation = "submerge ease-out 1s forwards";
