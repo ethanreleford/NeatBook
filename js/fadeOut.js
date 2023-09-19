09e738e47f21332f5a1803b16870eeb1579d8fa2
Ethan Releford <ethanrele@gmail.com>
2023-09-19 03:21:17 -0400

    copy small changes on host main when testing

--- js/fadeOut.js
+++ js/fadeOut.js
@@ -1,6 +1,6 @@
 
 function fadeOut(link){
-    document.getElementById("wrapper").style.animation = "fadeOut 1s";
+    document.getElementById("fadeinWrapper").style.animation = "fadeOut 1s";
     setTimeout(function()
     {
         document.getElementById("myForm").style.animation = "submerge ease-out 1s forwards";
