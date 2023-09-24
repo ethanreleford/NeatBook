442e9cae4d7c77fec6f22e226af8d1105fabb5e0
Declan Carter <declanpcarter@gmail.com>
2023-09-24 12:22:15 -0400

    Reformat Buttons on Table (Add Phone # Func.)

--- js/edit.js
+++ js/edit.js
@@ -1,21 +1,32 @@
 function edit(ContactID) {
     const row = document.getElementById(ContactID);
     const tds = row.querySelectorAll('td');
-  
+
     // Function to toggle the contenteditable attribute and style
     function toggleContentEditable() {
+      const saveBtn = row.querySelector('.editButton');
+      const span = saveBtn.querySelector('span');
       tds.forEach((td, index) => {
         if (index < tds.length - 1) {
           const isEditable = td.getAttribute('contenteditable') === 'true';
           td.contentEditable = isEditable ? 'false' : 'true';
           td.classList.toggle('editable', !isEditable); // Apply the 'editable' class
         }
+
+      if (tds[0].getAttribute('contenteditable') == 'true') {
+      span.className = "material-symbols-outlined";
+      span.innerHTML = "save";
+      }
+      else {
+        span.className = "material-icons";
+        span.innerHTML = "edit";
+      }
       });
     }
-  
+
     // Toggle contenteditable and apply styles
     toggleContentEditable();
-  
+
     if (tds[0].contentEditable == 'false') {
       const editRequest = {
         "FirstName": tds[0].textContent,
@@ -24,7 +35,7 @@ function edit(ContactID) {
         "PhoneNumber": tds[3].textContent,
         "Id": ContactID
       };
-  
+
       fetch(apiEndpointEdit, {
         method: 'POST',
         headers: {
@@ -36,22 +47,22 @@ function edit(ContactID) {
         .then(data => {
           const currentData = localStorage.getItem("localData");
           let userData;
-  
+
           if (currentData) {
             userData = Array.isArray(JSON.parse(currentData)) ? JSON.parse(currentData) : [];
           } else {
             userData = [];
           }
-  
+
           const index = userData.findIndex(item => item.ContactId === ContactID);
-  
+
           if (index !== -1) {
             userData[index] = {
               ...userData[index],
               ...editRequest
             };
           }
-  
+
           localStorage.setItem("localData", JSON.stringify(userData));
           update();
         })
@@ -60,6 +71,5 @@ function edit(ContactID) {
         });
     }
   }
-  
+
   const apiEndpointEdit = "http://poosd.xyz/LAMPAPI/UpdateContact.php";
-  
--- js/update.js
+++ js/update.js
@@ -1,19 +1,22 @@
 function update(){
     // Step 1: Retrieve the JSON string from localStorage
     const jsonUserData = JSON.parse(localStorage.getItem('localData'));
-   
+
     document.getElementById("contactsTableBody").innerHTML = '';
-    
+
     for (let user of jsonUserData){
        //console.log("contactId = " + user.ContactId);
        const newRow = document.createElement('tr');
        newRow.setAttribute('id', user.ContactId);
+       var emailPattern = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+       var phonePattern = "^[0-9]{3}-[0-9]{3}-[0-9]{4}$";
         newRow.innerHTML = `
-            <td contenteditable="false"><div class = "editInput">${user.FirstName}</div></td>
-            <td contenteditable="false"><div class = "editInput">${user.LastName}</div></td>
-            <td contenteditable="false"><div class = "editInput">${user.Email}</div></td>
-            <td contenteditable="false"><div class = "editInput">${user.PhoneNumber}</div></td>
-            <td>
+            <form>
+            <td contenteditable="false"><div class = "editInput" role="textbox" maxlength="50">${user.FirstName}</div></td>
+            <td contenteditable="false"><div class = "editInput" role="textbox" maxlength="50">${user.LastName}</div></td>
+            <td contenteditable="false"><div class = "editInput phoneNumber" role="textbox" maxlength="50" pattern=${emailPattern}>${user.Email}</div></td>
+            <td contenteditable="false"><div class = "editInput" role="textbox" maxlength="50">${user.PhoneNumber}</div></td>
+            <td style="display: flex;">
                 <button class="optionButton editButton">
                     <span class="material-icons">edit</span>
                 </button>
@@ -23,17 +26,22 @@ function update(){
                     </span>
                 </button>
             </td>
+            </form>
         `;
         contactsTableBody.appendChild(newRow);
-   
+
+        const phoneNumber = document.getElementById(user.ContactId).getElementsByClassName('phoneNumber')[0];
+        phoneNumber.addEventListener('keydown',enforceFormat);
+        phoneNumber.addEventListener('keyup',formatToPhone);
+
         document.getElementById(user.ContactId).getElementsByClassName('deleteButton')[0].addEventListener('click', function() {
-           const row = this.closest('tr');
-           Delete(row.id);
-       });
+            const row = this.closest('tr');
+            Delete(row.id);
+        });
 
        document.getElementById(user.ContactId).getElementsByClassName('editButton')[0].addEventListener('click', function() {
-        const row = this.closest('tr');
-        edit(row.id);
-    });
+            const row = this.closest('tr');
+            edit(row.id);
+        });
     }
    }
